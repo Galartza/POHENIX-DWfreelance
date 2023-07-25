2bca0b40ca1c25ce9117634a7e8b09f58ae3dba0
joa <jotaquin99g@gmail.com>
2023-07-25 19:28:46 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Función para abrir el menú móvil
+function openNav() {
+    document.getElementById("mobile-menu").style.width = "50%"; // Establecer el ancho del menú móvil al 50%
+}
+
+// Función para cerrar el menú móvil
+function closeNav() {
+    document.getElementById("mobile-menu").style.width = "0%"; // Establecer el ancho del menú móvil a 0%
+}
